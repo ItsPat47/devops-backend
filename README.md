d1b2ce5e7a64068908e0e62b98fe5d0bf2dd8954
ubuntu <chen.patrick47@gmail.com>
2025-03-19 14:18:10 +0000

    add dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # devops-backend
 this is the backend of the Devops project
+launch fastapi with
+fastapi dev main.py
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("yess")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+app = FastAPI()
+
+API_HOST = os.getenv("API_HOST", "localhost")
+API_PORT = int(os.getenv("API_PORT", 8000))
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {
+        "message": "Hello World",
+        }
+
+@app.get("/api/data")
+async def get_data():
+    return {"message": "vous êtes sur api/data"}
